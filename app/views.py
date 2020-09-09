0de706d049109808bcf1356aaf2a5fb5e27ba14d
harshadvali11 <66057460+harshadvali11@users.noreply.github.com>
2020-09-08 17:32:27 -0700

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app.models import *
+
+
+def display_topic(request):
+    #topics=Topic.objects.all()
+    topics=Topic.objects.get(topic_name='Dance')
+    return render(request,'display_topic.html',context={'topics':topics})
+
+
+def display_webpage(request):
+    #webpages=Webpage.objects.all()
+    webpages=Webpage.objects.filter(topic_name='Dance')
+
+    return render(request,'display_webpage.html',context={'webpages':webpages})
--- project13/urls.py
+++ project13/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('displaytopics/',views.display_topic,name='display_topic'),
+    path('displaywebpages/',views.display_webpage,name='display_webpage'),
 ]
